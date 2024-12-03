7dfcddd482b70a8cbdec6fd7aaa654d7d48cd0b5
litim smail <litimsmail@gmail.com>
2024-12-03 14:43:24 +0100

    Task 1

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -5,22 +5,21 @@ import java.util.List;
 
 public class Generics {
 
-    //TODO: Refactor Method-1
-    public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
+    public List<String> boxingMethod(String name) {
+       List<String> firstList = new ArrayList<>();
         firstList.add(name);
-        List secondList = new ArrayList<>();
+        List<String> secondList = new ArrayList<>();
         secondList.add(firstList);
         return secondList;
     }
 
-    //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+ 
+    public <ObjectT> ObjectT genericMethod(ObjectT data) {
         return data;
     }
 
-    //TODO: Refactor Method-3
-    public void cloneMethod(List consumer, List producer) {
+
+    public <T> void cloneMethod(List consumer, List producer) {
         consumer.addAll(producer);
     }
 
